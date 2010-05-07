2fcb76a8ec9d98c56e8a5ab5be4a946aaee16149
Milan Cermak <milan.cermak@gmail.com>
2010-05-07 03:12:29 +0200

    Changed the /contact and /about links to Slovak to improve SEO.

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('interviews.views',
                        (r'(?P<url>\w+\.\w+)/?$', 'interview'),
-                       (r'about/?$', 'about'),
-                       (r'contact/?$', 'contact'),
+                       (r'projekt/?$', 'about'),
+                       (r'kontakt/?$', 'contact'),
                        (r'/?$', 'index'),
                        )
 

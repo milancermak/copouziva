6dab88d1076032403d83c212d0da6e16d3d357dc
Milan Cermak <milan.cermak@gmail.com>
2010-05-12 23:37:41 +0200

    Adding RSS and Atom interview feeds.

---
+++ feeds.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.syndication import feeds
+from django.utils.feedgenerator import Atom1Feed
+from models import Person
+
+
+class RSSLatestInterviews(feeds.Feed):
+
+    link = '/'
+    title = 'Čo používa'
+    description = 'Nové rozhovory na Čo používa.'
+
+    def items(self):
+        return Person.objects.all()[:7] # last seven
+
+    def item_link(self, item):
+        return '/'+item.url+'/'
+
+class AtomLatestInterviews(RSSLatestInterviews):
+
+    feed_type = Atom1Feed
+    subtitle = RSSLatestInterviews.description
--- urls.py
+++ urls.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls.defaults import *
+from feeds import RSSLatestInterviews, AtomLatestInterviews
 
-urlpatterns = patterns('interviews.views',
+
+feeds = {
+    'rss': RSSLatestInterviews,
+    'atom': AtomLatestInterviews
+    }
+
+urlpatterns = patterns('',                       
+                       (r'feed/(?P<url>.*)/?$', 'django.contrib.syndication.views.feed',
+                        {'feed_dict': feeds}))
+
+
+urlpatterns += patterns('interviews.views',
                        (r'(?P<url>\w+\.\w+)/?$', 'interview'),
                        (r'projekt/?$', 'about'),
                        (r'kontakt/?$', 'contact'),
